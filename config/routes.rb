2e01c9e617e9dfc70770784d554f0188efe2ac42
Pramod Jacob <psjacob68@gmail.com>
2016-12-06 00:34:53 -0500

    added photo page HTML & CSS

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/timeline' => 'static_pages#timeline'
   get '/friends' => 'static_pages#friends'
   get '/about' => 'static_pages#about'
+  get '/photos' => 'static_pages#photos'
 
 end
