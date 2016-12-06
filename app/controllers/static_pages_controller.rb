3e4fac2292cce0c350993e3b4f697d7f73da285c
Pramod Jacob <psjacob68@gmail.com>
2016-12-05 23:58:59 -0500

    added friend page HTML & CSS

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
 
   def home
-    
+  end
+
+  def timeline
+  end
+
+  def friends
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get '/timeline' => 'static_pages#timeline'
+  get '/friends' => 'static_pages#friends'
 
 end
