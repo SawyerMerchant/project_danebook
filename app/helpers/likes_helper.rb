c588b2d23c522a506756a32b2599a655b3ad6e59
John Sawyer <sawyer@sawyermerchant.com>
2016-12-11 20:25:03 -0500

    Can like and unlike posts

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -11,7 +11,41 @@ module LikesHelper
   def like_count(post)
     post.user_likes.count
   end
+
+  # first three people who like a post
+  # currently outputs a mess
+  def who_likes(post)
+    links = []
+    post.user_likes.limit(3).each do |u|
+      links << "<%= link_to u.profile.full_name, user_path(u), class: 'btn btn-link like-button' %>"
+    end
+    links
+  end
+
+  def signed_in_user_likes(post)
+    if post.user_likes.include?(current_user)
+      "You"
+    end
+  end
+
+  def like_by(post)
+    unless post.user_likes.empty?
+      render partial: 'shared/liked_by', locals: {post: post}
+    end
+  end
+
+  def other_likes(post)
+
+  end
+
 end
+
+
+#TODO finish logic for rendering who likes a post
+# empty: nothing
+# signed_in_user: "you like this"
+# < 3, names of each
+# > 3, names of each plus count of remainder
 
 
 # Like.where(user_id: 16, post_id: 50)
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user, inverse_of: :profile
+
+  def full_name
+    self.first_name + ' ' + self.last_name
+  end
+
 end
